0c909812f98aa98bc2c440a0af56b8082e35f895
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-04-28 21:28:17 +0800

    Update variableReplace.py

--- files/variableReplace.py
+++ files/variableReplace.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pandas as pd
 from collections import Counter
@@ -55,4 +57,24 @@ for i in range(len(table)):
 print(table)
 
 
-table.to_excel('pollen files/results/process_output_quantid_neg_camera_noid/peaktableNEGout_Neg_noid_replace.xlsx',index=False,na_rep=np.nan)
+table.to_excel('pollen files/results/process_output_quantid_pos_camera_noid/peaktablePOSout_Pos_noid_replace.xlsx',index=False,na_rep=np.nan)
+
+data = pd.read_excel('pollen files/results/process_output_quantid_pos_camera_noid/peaktablePOSout_Pos_noid_replace.xlsx')
+
+targets = data.columns.values[1:]
+
+
+
+
+
+for i in range(len(data)):
+    temp = []
+    for j in targets:
+        temp.append(data[j][i])
+    for k in range(len(temp)):
+        temp[k] = math.isnan(temp[k])
+    if temp.count(True) >= len(temp) /2:
+        data = data.drop(i)
+
+print(data)
+data.to_excel('pollen files/results/process_output_quantid_pos_camera_noid/peaktablePOSout_Pos_noid_replace.xlsx',index=False,na_rep=np.nan)
