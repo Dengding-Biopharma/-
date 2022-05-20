ee2d09d12895234de1657328d1fcb063ecba4e4d
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-05-20 15:01:05 +0800

    1

--- files/2.concat_pos_neg.py
+++ files/2.concat_pos_neg.py
@@ -11,15 +11,13 @@ neg_columns = neg_data.columns.values
 
 for i in range(len(pos_columns)):
     if pos_columns[i] not in neg_columns:
-        neg_data.insert(i,pos_columns[i],np.nan)
+        neg_data.insert(i, pos_columns[i], np.nan)
 
 neg_columns = neg_data.columns.values
 
 for i in range(len(neg_columns)):
     if neg_columns[i] not in pos_columns:
-        pos_data.insert(i,neg_columns[i],np.nan)
-print(neg_data.columns.values)
-print(pos_data.columns.values)
+        pos_data.insert(i, neg_columns[i], np.nan)
 
 data = pd.concat([pos_data, neg_data], ignore_index=True)
 targets = data.columns.values[2:]
@@ -30,9 +28,9 @@ for i in range(len(data)):
         temp.append(data[j][i])
     for k in range(len(temp)):
         temp[k] = math.isnan(temp[k])
-    if temp.count(True) > len(temp) /2:
+    if temp.count(True) > len(temp) / 2:
         data = data.drop(i)
 
 print(data)
 
-data.to_excel('ad files/peaktableBOTHout_BOTH_noid_replace.xlsx',index=False,na_rep=np.nan)
+data.to_excel('ad files/peaktableBOTHout_BOTH_noid_replace.xlsx', index=False, na_rep=np.nan)
--- smile_to_id.py
+++ smile_to_id.py
@@ -5,106 +5,7 @@ from rdkit import Chem
 import pandas as pd
 
 table = pd.read_csv('hmdb_metabolites.csv', sep='\t')
-# count = 0
-print(table.columns.values)
-# for i in range(len(table)):
-#     if type(table['kegg_id'][i]) != float:
-#         count += 1
-# print(count)
-# print(table['inchi'])
-
-# import math
-# from math import nan
-#
-# import numpy as np
-# from scipy.stats import ttest_ind
-# from sklearn.impute import SimpleImputer
-#
-# data = pd.read_excel('files/ad files/peaktablePOSout_POS_noid_more_puring_mean_full.xlsx')
-#
-# targets = data.columns.values[1:]
-#
-# print(targets)
-#
-# saved_label = data['dataMatrix'].values
-# print(saved_label)
-# del data['dataMatrix']
-# data_impute = data.values
-# for i in range(data_impute.shape[1]):
-#     data_impute[:, i] = data_impute[:, i] / np.sum(data_impute[:, i])
-# print(data_impute)
-#
-# normalized_data_impute = data_impute
-# print(normalized_data_impute.shape)
-# ad_index=[]
-# hc_index=[]
-# for i in range(len(targets)):
-#     if "AD" in targets[i]:
-#         ad_index.append(i)
-#     else:
-#         hc_index.append(i)
-#
-#
-# normalized_data_impute_ad = []
-# for index in ad_index:
-#     normalized_data_impute_ad.append(normalized_data_impute[:,index].T)
-# normalized_data_impute_ad = np.array(normalized_data_impute_ad)
-#
-# normalized_data_impute_hc =[]
-# for index in hc_index:
-#     normalized_data_impute_hc.append(normalized_data_impute[:,index].T)
-# normalized_data_impute_hc = np.array(normalized_data_impute_hc)
-#
-#
-# top_k = 20
-# p_list =[]
-# for i in range(normalized_data_impute_ad.shape[1]):
-#     t,p = ttest_ind(normalized_data_impute_ad[:,i:i+1],normalized_data_impute_hc[:,i:i+1],equal_var=True)
-#     p_list.append(p[0])
-# p_list = np.array(p_list)
-# count = 0
-# for p in p_list:
-#     if p < 0.05:
-#         count +=1
-#
-# top_k_index = p_list.argsort()[::-1][len(p_list)-count:]
-# #top_k_index = p_list.argsort()[::-1][:]
-# print(top_k_index)
-# print(count)
-# df = pd.DataFrame()
-# df['P']=p_list[top_k_index]
-# df['name']=saved_label[top_k_index]
-# df.to_excel('pos_significant.xlsx', index=False)
-# data = pd.read_excel('pos_significant.xlsx')
-# print(data)
-#
-# dup_row = data.duplicated(subset=['name'],keep=False)
-# print(dup_row)
-#
-# dup_df = data[dup_row]
-# for i in range(len(data)):
-#     if dup_row[i] == True:
-#         data = data.drop(i)
-# print(data)
-# dup_df=dup_df.sort_values(by='name')
-#
-# print(dup_df)
-#
-# dup_df.to_excel('temp.xlsx',index=False)
-# dup_df = pd.read_excel('temp.xlsx')
-# print(dup_df)
-# temp = []
-# for i in range(len(dup_df)):
-#     if len(temp) == 0 or dup_df['name'][i] != temp[0]:
-#         temp = [dup_df['name'][i], dup_df['P'][i],i]
-#     elif dup_df['name'][i] == temp[0] and dup_df['P'][i] < temp[1]:
-#         dup_df = dup_df.drop(temp[2])
-#         temp =[dup_df['name'][i],dup_df['P'][i],i]
-#     elif dup_df['name'][i] == temp[0] and dup_df['P'][i] > temp[1]:
-#         dup_df = dup_df.drop(i)
-#
-# print(len(dup_df))
-finalDF = pd.read_excel('pos_significant.xlsx')
+finalDF = pd.read_excel('BOTH_significant.xlsx')
 
 
 finalDF['accession'] = np.nan
@@ -123,14 +24,14 @@ for k in range(len(finalDF)):
     # print(name,inchiKeys)
     for i in range(len(table['name'].values)):
         if name == table['name'].values[i][2:-1]:
-            print('match!!!!!!!',table['name'].values[i][2:-1])
+            print('match name!!!!!!!',table['name'].values[i][2:-1])
             ids.append(table['accession'].values[i])
             finalDF['inchikey'][k] = table['inchikey'].values[i]
             finalDF['accession'][k] = table['accession'].values[i]
             print(ids[-1])
             break
         elif inchikey == str(table['inchikey'][i]):
-            print('match!!!!!!!',str(table['inchikey'][i]))
+            print('match inchikey!!!!!!!',str(table['inchikey'][i]))
             ids.append(table['accession'].values[i])
             finalDF['inchikey'][k] = table['inchikey'].values[i]
             finalDF['accession'][k] = table['accession'].values[i]
@@ -141,4 +42,4 @@ print(len(ids))
 for id in ids:
     print(id)
 print(finalDF)
-finalDF.to_excel('pos_hmdb_id_smile_inchikey.xlsx',index=False,na_rep=np.nan)
+finalDF.to_excel('both_hmdb_id_smile_inchikey.xlsx',index=False,na_rep=np.nan)
